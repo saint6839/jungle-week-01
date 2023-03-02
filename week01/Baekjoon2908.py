97a7ad6f0083916918eb5c1d957589656cd78bc0
saint6839 <saint6839@gmail.com>
2023-03-03 00:38:44 +0900

    refactor: 백준2908 solved

--- week01/Baekjoon2908.py
+++ week01/Baekjoon2908.py
@@ -1,15 +1,5 @@
 A, B = input().split()
 
-a_list = list(A)
-b_list = list(B)
-temp = ''
-for a in reversed(A):
-    temp += a
-newA = int(temp)
-
-temp = ''
-for b in reversed(B):
-    temp += b
-newB = int(temp)
-
+newA = int(A[::-1])
+newB = int(B[::-1])
 print(max(newA, newB))
