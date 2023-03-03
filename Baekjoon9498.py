13fb31cf1a4ab3065fe48d8c14544f883a647b89
seojuntaek <jwutn908@gmail.com>
2023-03-03 10:15:30 +0900

    Baekjoon9498

---
+++ Baekjoon9498.py
@@ -0,0 +1,12 @@
+grade = int(input())
+if grade >= 90 and grade <= 100:
+    print('A')
+elif grade >= 80:
+    print('B')
+elif grade >= 70:
+    print('C')
+elif grade >= 60:
+    print('D')
+else:
+    print('F')
+  
