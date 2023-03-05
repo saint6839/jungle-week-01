bd6e741adcf65f3ffa5543f95a65d5a662121afe
saint6839 <saint6839@gmail.com>
2023-03-05 22:00:03 +0900

    feat: 백준15664 solved additional

---
+++ week01/Baekjoon15664.py
@@ -0,0 +1,33 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+numbers = list(map(int, sys.stdin.readline().split()))
+
+numbers.sort()
+temps = []
+
+
+isVisits = [False for i in range(N)]
+results = set()
+
+def dfs(depth, index):
+    if depth == M:
+        results.add(tuple(temps))
+        return
+
+    for i in range(index, N):
+        if not isVisits[i]:
+            isVisits[i] = True
+            temps.append(numbers[i])
+            dfs(depth+1, i)
+            temps.pop()
+            isVisits[i] = False
+
+dfs(0, 0)
+results = list(map(list,results))
+results.sort()
+for result in results:
+    for r in result:
+        print(r, end=' ')
+    print()
+
